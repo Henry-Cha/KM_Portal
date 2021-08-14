73d9dd435cbf9ab85d778def850b2d6caceedfab
차셩 <sungwon4728@naver.com>
2021-08-14 15:23:30 +0000

    EDIT news

--- news/api.py
+++ news/api.py
@@ -3,16 +3,114 @@ from bs4 import BeautifulSoup
 
 
 def check_standard():
-    data = []
+    data = {}
     url = "https://www.kmu.ac.kr/uni/main/page.jsp?mnu_uid=143&"
     res = requests.get(url)
     xml = res.text
+    n = 1
 
     soup = BeautifulSoup(xml, 'html.parser')
     datalist = soup.find('tbody').findAll('tr')
     for i in datalist:
-        data.append(i.find('td', class_='subject').text)
-        data.append(i.find('td', class_='subject').find('a')["href"])
-        data.append(i.find('td', class_='writer').text)
-        data.append(i.find('td', class_='date').text)
+        data[f"st_sub{n}"] = (i.find('td', class_='subject').text)
+        data[f"st_href{n}"] = (i.find('td', class_='subject').find('a')["href"])
+        data[f"st_wrt{n}"] = (i.find('td', class_='writer').text)
+        data[f"st_date{n}"] = (i.find('td', class_='date').text)
+        n = n+1
+        if n is 10:
+            break;
+    return data
+
+def check_academic():
+    data = {}
+    url = "https://www.kmu.ac.kr/uni/main/page.jsp?mnu_uid=144&"
+    res = requests.get(url)
+    xml = res.text
+    n = 1
+
+    soup = BeautifulSoup(xml, 'html.parser')
+    datalist = soup.find('tbody').findAll('tr')
+    for i in datalist:
+        data[f"ac_sub{n}"] = (i.find('td', class_='subject').text)
+        data[f"ac_href{n}"] = (i.find('td', class_='subject').find('a')["href"])
+        data[f"ac_wrt{n}"] = (i.find('td', class_='writer').text)
+        data[f"ac_date{n}"] = (i.find('td', class_='date').text)
+        n = n+1
+        if n is 10:
+            break;
+    return data
+
+def check_award():
+    data = {}
+    url = "https://www.kmu.ac.kr/uni/main/page.jsp?mnu_uid=145&"
+    res = requests.get(url)
+    xml = res.text
+    n = 1
+
+    soup = BeautifulSoup(xml, 'html.parser')
+    datalist = soup.find('tbody').findAll('tr')
+    for i in datalist:
+        data[f"aw_sub{n}"] = (i.find('td', class_='subject').text)
+        data[f"aw_href{n}"] = (i.find('td', class_='subject').find('a')["href"])
+        data[f"aw_wrt{n}"] = (i.find('td', class_='writer').text)
+        data[f"aw_date{n}"] = (i.find('td', class_='date').text)
+        n = n+1
+        if n is 10:
+            break;
+    return data
+
+def check_recruit():
+    data = {}
+    url = "https://www.kmu.ac.kr/uni/main/page.jsp?mnu_uid=147&"
+    res = requests.get(url)
+    xml = res.text
+    n = 1
+
+    soup = BeautifulSoup(xml, 'html.parser')
+    datalist = soup.find('tbody').findAll('tr')
+    for i in datalist:
+        data[f"cr_sub{n}"] = (i.find('td', class_='subject').text)
+        data[f"cr_href{n}"] = (i.find('td', class_='subject').find('a')["href"])
+        data[f"cr_wrt{n}"] = (i.find('td', class_='writer').text)
+        data[f"cr_date{n}"] = (i.find('td', class_='date').text)
+        n = n+1
+        if n is 10:
+            break;
+    return data
+
+def check_job():
+    data = {}
+    url = "https://www.kmu.ac.kr/uni/main/page.jsp?mnu_uid=3445&"
+    res = requests.get(url)
+    xml = res.text
+    n = 1
+
+    soup = BeautifulSoup(xml, 'html.parser')
+    datalist = soup.find('tbody').findAll('tr')
+    for i in datalist:
+        data[f"jo_sub{n}"] = (i.find('td', class_='subject').text)
+        data[f"jo_href{n}"] = (i.find('td', class_='subject').find('a')["href"])
+        data[f"jo_wrt{n}"] = (i.find('td', class_='writer').text)
+        data[f"jo_date{n}"] = (i.find('td', class_='date').text)
+        n = n+1
+        if n is 10:
+            break;
+    return data
+
+def check_nonsub():
+    data = {}
+    url = "https://story.kmu.ac.kr/user/Ep/EpMng010L.do?CURRENT_MENU_CODE=MENU0052&TOP_MENU_CODE=MENU0004&PRM_ORDER=2"
+    res = requests.get(url)
+    xml = res.text
+    n = 1
+
+    soup = BeautifulSoup(xml, 'html.parser')
+    #datalist = soup.find('ul', class_="extra_card").findAll('li')
+    # for i in datalist:
+    #     data[f"non_title{n}"] = (i.find('div', class_="cardInfo").find('h4').text)
+    #     data[f"non_date{n}"] = (i.find('div', class_="cardInfo").find('strong').text)
+    #     data[f"non_mem{n}"] = (i.find('div', class_="cardInfo").find('em').text)
+    #     n = n+1
+    #     if n is 10:
+    #         break;
     return data
--- news/views.py
+++ news/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from .api import check_standard
+from .api import *
 
 def index(request):
-    data = check_standard()
-    return render(request, 'news/news.html', {'sub1':data[0],'href1':data[1],'wrt1':data[2],'date1':data[3],'sub2':data[4],'href2':data[5],'wrt2':data[6],'date2':data[7],'sub3':data[8],'href3':data[9],'wrt3':data[10],'date3':data[11],'sub4':data[12],'href4':data[13],'wrt4':data[14],'date4':data[15],'sub5':data[16],'href5':data[17],'wrt5':data[18],'date5':data[19]})
+    st_data = check_standard()
+    ac_data = check_academic()
+    aw_data = check_award()
+    cr_data = check_recruit()
+    jo_data = check_job()
+    non_data = check_nonsub()
+    return render(request, 'news/news.html',{**st_data,**ac_data,**aw_data,**cr_data,**jo_data,**non_data})
